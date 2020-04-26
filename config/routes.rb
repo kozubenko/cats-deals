aadeb3a6ef7510c12d7a571693bd825dee30e9d1
andriikozubenko <andrii.kozubenko@gmail.com>
2020-04-26 11:30:52 +0300

    change controllers, add cats deals controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "requests#new"
 
-  resource :request, only: %i[create new] do
-    get :result
-  end
+  resource :request, only: %i[create new]
+  resources :cats_deals, only: %i[index]
 end
